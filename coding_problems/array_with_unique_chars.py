d789bd4fc099c5eef66269f293f143d0a5860ecc
GeorgeKesaev <kesaev@gmail.com>
2019-04-06 19:17:54 +0300

    first version

---
+++ coding_problems/array_with_unique_chars.py
@@ -0,0 +1,56 @@
+import unittest
+
+
+class Solution:
+    def is_unique_chars(self, s: str) -> bool:
+        if len(s) > 256:
+            return False
+        image = [False] * 256
+        for c in s:
+            if image[ord(c)]:
+                return False
+            image[ord(c)] = True
+        return True
+
+    def is_unique_chars_bool(self, s: str) -> bool:
+        if len(s) > 256:
+            return False
+        res = 0
+        for c in s:
+            if res & (1 << ord(c) - ord('A')):
+                return False
+            res |= 1 << ord(c) - ord('A')
+        return True
+
+
+# class TestSolution(unittest.TestCase):
+class TestSolution(unittest.TestCase):
+    sol = Solution()
+
+    def is_unique_chars_test_positive(self):
+        self.assertTrue(self.sol.is_unique_chars('abcdef'))
+        self.assertTrue(self.sol.is_unique_chars('f'))
+        self.assertTrue(self.sol.is_unique_chars('bahfs'))
+
+    def is_unique_chars_test_negative(self):
+        self.assertFalse(self.sol.is_unique_chars('bahfss'))
+        self.assertFalse(self.sol.is_unique_chars('AZA'))
+        self.assertFalse(self.sol.is_unique_chars('kaaaa'))
+
+    def is_unique_chars_bool_test_true(self):
+        self.assertTrue(self.sol.is_unique_chars_bool('f'))
+        self.assertTrue(self.sol.is_unique_chars_bool('bahfs'))
+
+    def is_unique_chars_bool_test_false(self):
+        self.assertFalse(self.sol.is_unique_chars_bool('bahfss'))
+        self.assertFalse(self.sol.is_unique_chars_bool('AZA'))
+        self.assertFalse(self.sol.is_unique_chars_bool('kaaaa'))
+
+
+if __name__ == '__main__':
+    # unittest.main()
+    t1 = TestSolution()
+    t1.is_unique_chars_test_positive()
+    t1.is_unique_chars_test_negative()
+    t1.is_unique_chars_bool_test_true()
+    t1.is_unique_chars_bool_test_false()
