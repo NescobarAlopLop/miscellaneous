bbd02c83df969bac9d06c34222ec8cd5b27313dc
george <kesaev@gmail.com>
2021-05-31 22:25:41 +0300

    maze traversal with 2 optional steps

---
+++ coding_problems/reach_the_end_in_time.py
@@ -0,0 +1,72 @@
+def reachTheEnd(grid, maxTime):
+    if reachEndHelper(
+        grid=grid,
+        max_time=maxTime,
+        start_x=0,
+        start_y=0,
+    ):
+        return 'Yes'
+
+    return 'No'
+
+
+def reachEndHelper(grid, max_time, start_x, start_y) -> bool:
+    if max_time < 0:
+        return False
+
+    if start_x == len(grid[0]) - 1 and start_y == len(grid) - 1:
+        return True
+
+    if start_y + 1 < len(grid):
+        if grid[start_y + 1][start_x] == '.':
+            can_reach1 = reachEndHelper(
+                grid=grid,
+                max_time=max_time - 1,
+                start_x=start_x,
+                start_y=start_y + 1,
+            )
+            if can_reach1:
+                return True
+    elif start_x + 1 < len(grid[0]):
+        if grid[start_y][start_x + 1] == '.':
+            can_reach2 = reachEndHelper(
+                grid=grid,
+                max_time=max_time - 1,
+                start_x=start_x + 1,
+                start_y=start_y,
+            )
+            if can_reach2:
+                return True
+
+    return False
+
+
+def next_steps(
+    grid,
+    location,
+):
+    for possible_edge in [
+        (0, 1),
+        (1, 0),
+    ]:
+        next_step = [sum(x) for x in zip(location, possible_edge)]
+        if next_step[0] < len(grid[0]) and next_step[1] < len(grid):
+            yield next_step[0], next_step[1]
+
+    return
+
+
+if __name__ == '__main__':
+    grid = [
+        '.#.',
+        '.#.',
+        '...'
+    ]
+
+    steps = len(grid) * 2 - 2
+    print(
+        reachTheEnd(
+            grid=grid,
+            maxTime=steps,
+        )
+    )
