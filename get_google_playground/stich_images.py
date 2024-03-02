88c37a7452aaf5ccfbe9fa3a2abed3efdad1a6fc
nescobaraloplop <17679385+NescobarAlopLop@users.noreply.github.com>
2024-03-02 17:57:37 +0200

    revert multithreading for svg to png conversion

--- get_google_playground/stich_images.py
+++ get_google_playground/stich_images.py
@@ -1,34 +1,36 @@
 import concurrent.futures
 import os
-import threading
 from collections import namedtuple
 
 import cairosvg
 import requests
 from PIL import Image
 
-lock = threading.Lock()
-
 ImageData = namedtuple('ImageData', ['image', 'row', 'col'])
+
+
+def download_image(base_url, row, col, download_dir):
+    file_path = create_svg_file_path(download_dir, row, col)
+
+    if not os.path.exists(file_path):
+        url = f"{base_url}/{row:02d}_{col:02d}.svg"
+        print(f"Downloading {url}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(file_path, "wb") as svg_file:
+                svg_file.write(response.content)
+        else:
+            print(f"Failed to download {url}")
 
 
 def download_images(base_url, rows, columns, download_dir):
     if not os.path.exists(download_dir):
         os.makedirs(download_dir)
 
-    for row in range(rows):
-        for col in range(columns):
-            file_path = os.path.join(download_dir, f"{row:02d}_{col:02d}.svg")
-
-            if not os.path.exists(file_path):
-                url = f"{base_url}/{row:02d}_{col:02d}.svg"
-                print(f"Downloading {url}")
-                response = requests.get(url)
-                if response.status_code == 200:
-                    with open(file_path, "wb") as svg_file:
-                        svg_file.write(response.content)
-                else:
-                    print(f"Failed to download {url}")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for row in range(rows):
+            for col in range(columns):
+                executor.submit(download_image, base_url, row, col, download_dir)
 
 
 def stitch_images(download_dir, rows, columns, output_path):
@@ -53,12 +55,11 @@ def stitch_images(download_dir, rows, columns, output_path):
 def svg_to_png(columns, download_dir, rows) -> list[ImageData]:
     images: list[ImageData] = []
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_to_image = create_future_to_image_dict(executor, rows, columns, download_dir)
-        for future in concurrent.futures.as_completed(future_to_image):
-            row, col = future_to_image[future]
+    for row in range(rows):
+        for col in range(columns):
+            print(f"Converting {row:02d}_{col:02d}.svg to PNG")
             try:
-                image = future.result()
+                image = convert_svg_to_png(create_svg_file_path(download_dir, row, col))
             except Exception as exc:
                 print(f'Generated an exception: {exc}')
             else:
@@ -80,11 +81,12 @@ def convert_svg_to_png(file_path):
     try:
         if os.path.exists(file_path):
             png_file_path = file_path.replace('.svg', '.png')
-            with lock:
-                cairosvg.svg2png(url=file_path, write_to=png_file_path)
+            cairosvg.svg2png(url=file_path, write_to=png_file_path)
             image = Image.open(png_file_path)
 
             return image
+        else:
+            print(f"{file_path} does not exist")
     except Exception as e:
         print(f"An error occurred while converting {file_path} to PNG: {e}")
 
